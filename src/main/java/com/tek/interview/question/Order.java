832c86d220812205c5fbcb5de4a0fca0fa8ce9f5
sreenivasula mula <sreenivasula.mula@capitalone.com>
2017-07-15 14:08:30 -0400

    removed commented code

--- src/main/java/com/tek/interview/question/Order.java
+++ src/main/java/com/tek/interview/question/Order.java
@@ -23,13 +23,3 @@ public class Order {
         return orderLines.get(i);
     }   
 }
-
-
-/*
- * Copyright 2017 Capital One Financial Corporation All Rights Reserved.
- * 
- * This software contains valuable trade secrets and proprietary information of
- * Capital One and is protected by law. It may not be copied or distributed in
- * any form or medium, disclosed to third parties, reverse engineered or used in
- * any manner without prior written authorization from Capital One.
- */
